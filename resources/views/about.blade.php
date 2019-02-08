0e3b95facff7bcc7001e7245de8b757de549da11
Olexandr Plakhotniuk <plaxotpos@gmail.com>
2019-02-08 18:29:24 +0200

    new part. Create databases

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>about</title>
-</head>
-<body>
-    <h1>About</h1>
-</body>
-</html>
+@extends('layout')
+
+@section('title', 'About us')
+
+@section('content')
+
+	<h1>About</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate debitis dolorem esse fugiat nisi
+		officia, praesentium reiciendis sint veniam.</p>
+
+@endsection
