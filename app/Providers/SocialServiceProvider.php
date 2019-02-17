64308475d61507dcb8cafcd6a4a6650d9569f668
Olexandr Plakhotniuk <plaxotpos@gmail.com>
2019-02-17 15:01:04 +0200

    before auth-example

---
+++ app/Providers/SocialServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\Twitter;
+
+class SocialServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    	$this->app->singleton(Twitter::class, function () {
+    		return new Twitter(config('services.twitter.secret'));
+	    });
+    	/*$this->app->bind(
+		    \App\Repositories\UserRepository::class,
+		    \App\Repositories\DbUserRepository::class
+	    );*/
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
